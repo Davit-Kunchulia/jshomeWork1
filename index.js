9d02476d5ae98e3119ced27c2137d1192fcc114d
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2024-01-14 18:46:26 +0400

    finished

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+// 1
+let asi = 100
+
+if (asi < 50) {
+    console.log("ნაკლებია 50-ზე")
+}
+else if (asi > 20) {
+    console.log("მეტია 20-ზე")
+}
+else {
+    console.log("შეცდომა")
+}
+
+
+// 2
+let mariam = "მარიამ"
+
+if (mariam = "მარიამ") {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+
+// 3
+let mr = "მარიამ"
+switch (mr) {
+    case "მარიამ":
+        console.log("true");
+        break;
+    default:
+        console.log("false");
+        break;
+
+}
+
+
+// 4
+let nebismieri = "ნებისმიერი"
+
+console.log(typeof nebismieri)
+
+
+// 5
+let number = 10
+console.log(String(number))
